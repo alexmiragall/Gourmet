31436c13c8a955c39e39c318e6701247693b5478
amiragall <amiragall@tuenti.com>
2016-03-10 13:16:09 +0100

    added butter knife and picasso + events adapter

---
+++ app/src/main/java/com/tuenti/gourmet/models/Event.java
@@ -0,0 +1,39 @@
+package com.tuenti.gourmet.models;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Copyright (c) Tuenti Technologies. All rights reserved.
+ */
+public class Event implements Serializable {
+	private Restaurant restaurant;
+	private Date date;
+	private User owner;
+	private String comment;
+	private List<User> occupants;
+
+	public Event(Restaurant restaurant, Date date, User owner, String comment) {
+		this.restaurant = restaurant;
+		this.date = date;
+		this.owner = owner;
+		this.comment = comment;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+}
